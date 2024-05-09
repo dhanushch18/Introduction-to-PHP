df11ee406512f59e333b5484507a9c4805abb88f
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-05-09 23:01:29 +0530

    Create 01.intro.php

---
+++ 01.intro.php
@@ -0,0 +1,25 @@
+<?php
+
+    echo"I luv u <br>";
+    echo"Dhanush ch <br>";
+    echo"ch17 <br>";
+
+    // This is a comment
+    /* this
+    is a
+    page
+    */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <button>Order</button>
+</body>
+</html>
