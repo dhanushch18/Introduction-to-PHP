10f5afb3332252397a7c393f0782b9918f0df547
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-07-08 22:26:12 +0530

    Create 02.variables.php

---
+++ 02.variables.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$x = 5;
+$y = "John";
+$z= "Pizza";
+
+echo $x;
+echo "<br>";
+echo $y;
+echo "<br>";
+
+echo "You have ordered {$x} x {$z}";
+?>
+
+</body>
+</html>
+
+output 
+
+ 5
+John
+You have ordered 5 x Pizza 
