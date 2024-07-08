7b462950ccf2995853991b9f207160c0a59eda47
Dhanush CH <86602109+dhanushch18@users.noreply.github.com>
2024-07-08 23:08:04 +0530

    Create 03.variable1.php

---
+++ 03.variable1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<pre>
+
+<?php
+var_dump(5);
+var_dump("John");
+var_dump(3.14);
+var_dump(true);
+var_dump([2, 3, 56]);
+var_dump(NULL);
+?>
+
+</pre>
+
+</body>
+</html>
+
+
+int(5)
+string(4) "John"
+float(3.14)
+bool(true)
+array(3) {
+  [0]=>
+  int(2)
+  [1]=>
+  int(3)
+  [2]=>
+  int(56)
+}
+NULL
